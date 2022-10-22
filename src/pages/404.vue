2cb499f8b6f9470ba4bf2eee977dd6afd5feb572
Aliaksei Hrynko <aliaksei.hrynko@gmail.com>
2022-10-22 12:10:01 +0200

    Add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout class="not-found-page">
+    <div class="container-s">
+      <h1>Page not found</h1>
+
+      <p>The page doesn't exist or was removed</p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Page not found',
+  },
+}
+</script>
+
+<style lang="scss">
+.not-found-page {
+  text-align: center;
+
+  h1 {
+    margin-top: calc(var(--gap) * 1.5);
+  }
+}
+</style>
